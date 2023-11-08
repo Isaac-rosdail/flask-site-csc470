f43c3b0181df038d05af7fbad64d0630147bb43c
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-07 18:32:19 -0600

    f

--- app/app.py
+++ app/app.py
@@ -20,6 +20,7 @@ class User(db.Model):
     email = db.Column(db.String(30), unique=True) # email must be unique
     password = db.Column(db.String(30))
 
+# Diff credential levels, button on forms for selecting department
 
 # Init db & tables if needed
 with app.app_context():

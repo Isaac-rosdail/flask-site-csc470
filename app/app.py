b0c7920a5f208b32f6b185a4138fc2cca6de3a60
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-13 23:41:12 -0600

    Fix login issue, will fix routes accordingly

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy    # Init SQLAlchemy
 from forms import RegisterForm, LoginForm  # Import register/login forms
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import flash ## debugging login stuff
 
 app = Flask(__name__)
@@ -9,7 +9,6 @@ app.secret_key = 'corn'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Not needed?
 app.config['SECRET_KEY'] = 'corn'  # For Flask_WTF form(s)
-
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
@@ -22,7 +21,7 @@ def load_user(user_id):
 
 
 # Define models (User, Ticket)
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # name = db.Column(db.String(30), unique=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
@@ -55,6 +54,7 @@ def home():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()  # Check if user is in db first
+        flash(f"Password: {form.password.data}")
         if user and user.password == form.password.data:
             login_user(user)
             flash('Login successful', 'success')

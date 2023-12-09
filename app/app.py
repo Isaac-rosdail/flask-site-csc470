8cce148c0216b0fec3c3ae887d42d63be24c2526
TheAlkemist <Tobechrs941@gmail.com>
2023-12-09 16:14:23 -0600

    Created Dashboard for users/admin/staff Implemented Deletion of Users for admin Implemented table sort and search Trying to see if maybe a rough version of comments can be implemented

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
-from forms import RegisterForm, LoginForm, TicketForm      # Import forms from forms.py
+from forms import RegisterForm, LoginForm, TicketForm  # Import forms from forms.py
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy    # Init SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy  # Init SQLAlchemy
+from flask_paginate import Pagination
 
 app = Flask(__name__)
 app.secret_key = 'corn'
@@ -15,16 +16,18 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "home"
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, user_id)
+
 
 # Define models (User, Ticket)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
     username = db.Column(db.String(30), nullable=False, unique=True)
-    email = db.Column(db.String(30), unique=True) # email must be unique
+    email = db.Column(db.String(30), unique=True)  # email must be unique
     password = db.Column(db.String(30), nullable=False)
     role = db.Column(db.Integer)
     dept = db.Column(db.String(30))
@@ -32,7 +35,23 @@ class User(UserMixin, db.Model):
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    created_by = db.Column(db.String(30)) # get dept from finding user based on their user_id?
+    created_by = db.Column(db.String(30))  # get dept from finding user based on their user_id?
+    assigned_to = db.Column(db.String(30))
+    status = db.Column(db.String(30))
+    priority = db.Column(db.String(30))
+    dept = db.Column(db.String(30))
+    title = db.Column(db.String(30))
+    description = db.Column(db.String(100))
+    location = db.Column(db.String(30))
+    attachment = db.Column(db.String(30))
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ticket_id = db.Column
+    created_by = db.Column(db.String(30))  # get dept from finding user based on their user_id?
+    assigned_to = db.Column(db.String(30))
+    status = db.Column(db.String(30))
+    priority = db.Column(db.String(30))
     dept = db.Column(db.String(30))
     title = db.Column(db.String(30))
     description = db.Column(db.String(100))
@@ -48,6 +67,8 @@ with app.app_context():
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = LoginForm()
@@ -58,6 +79,8 @@ def home():
             login_user(user)
             return redirect(url_for('dashboard'))
     return render_template("home.html", form=form)
+
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -75,48 +98,100 @@ def register():
         return redirect(url_for('home'))
 
     return render_template('register.html', form=form)
+
+
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-
     return render_template('dashboard.html')
+
+
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+def filter_tickets(username,filter_type):
+    if filter_type=='created':
+        ticket_list = Ticket.query.filter_by(created_by=username).all()
+        #print(ticket_list[0].created_by)
+        return ticket_list
+    elif filter_type=='assigned':
+        ticket_list = Ticket.query.filter_by(assigned_to=username).all()
+        return ticket_list
+    else:
+        if current_user.role==2:
+            ticket_list = Ticket.query.all()
+        else:
+            ticket_list = Ticket.query.filter_by(dept=current_user.dept).all()
+    return ticket_list
+
+# Pagination settings
+PER_PAGE = 5  # Number of tickets per page
+
 @app.route("/tickets", methods=['GET', 'POST'])
 @login_required
 def tickets():
-    if current_user.role == 0:
-        ticket_list = Ticket.query.filter_by(created_by=current_user.name).all()  # Grab only the user's own ticket(s)
-    elif current_user.role == 1:
-        ticket_list = Ticket.query.filter_by(dept=current_user.dept).all()
-    else:
-        ticket_list = Ticket.query.all()
+    
+    username = request.args.get('username','admin')
+    filter_type = request.args.get('filter_type', 'all')
+    user_tickets = filter_tickets(username, filter_type)
+    
+    page = int(request.args.get('page', 1))
+    start = (page - 1) * PER_PAGE
+    end = start + PER_PAGE
+    pagination = Pagination(page=page, total=len(user_tickets), per_page=PER_PAGE, css_framework='bootstrap4')
+    
+    #print(user_tickets[0])
+    return render_template('tickets.html', tickets=user_tickets[start:end],pagination=pagination)
 
-    return render_template("tickets.html", tickets=ticket_list)
+@app.route("/users", methods=['GET', 'POST'])
+@login_required
+def users():
+    user_list = User.query.all()
+    
+    page = int(request.args.get('page', 1))
+    start = (page - 1) * PER_PAGE
+    end = start + PER_PAGE
+    pagination = Pagination(page=page, total=len(user_list), per_page=PER_PAGE, css_framework='bootstrap4')
+    
+    #print(user_tickets[0])
+    return render_template('users.html', users=user_list)
+
+
 @app.route("/submit_ticket", methods=['GET', 'POST'])
 @login_required
 def submit_ticket():
     form = TicketForm()
 
+    if current_user.role==0:
+        del form.priority
+        del form.status
+
     # Pre-fill 'created_by' field
     if request.method == 'GET':
-        form.created_by.data = current_user.name
+        form.created_by.data = current_user.username
 
     if form.validate_on_submit():
         # Store form data as new ticket
         new_ticket = Ticket(created_by=form.created_by.data,
                             title=form.title.data,
+                            assigned_to = form.assigned_to.default,
+                            status=getattr(form, 'status', None) and form.status.data or 'Open',
+                            priority=getattr(form, 'priority', None) and form.priority.data or 'Low',
                             description=form.description.data,
                             location=form.location.data,
                             attachment=form.attachment.data)
+        
         db.session.add(new_ticket)
         db.session.commit()
         return redirect(url_for('tickets'))
 
     return render_template("submit_ticket.html", form=form)
+
+
 @app.route('/edit_ticket/<int:ticket_id>', methods=['GET', 'POST'])
 @login_required
 def edit_ticket(ticket_id):
@@ -127,6 +202,9 @@ def edit_ticket(ticket_id):
     if form.validate_on_submit():
         ticket.dept = form.dept.data
         ticket.title = form.title.data
+        ticket.assigned_to = form.assigned_to.data
+        ticket.status = form.status.data
+        ticket.priority = form.priority.data
         ticket.description = form.description.data
         ticket.location = form.location.data
         ticket.attachment = form.attachment.data
@@ -135,6 +213,55 @@ def edit_ticket(ticket_id):
         return redirect(url_for('tickets'))
 
     return render_template('edit_ticket.html', form=form)
+
+@app.route('/_user/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)  # Grab a user based on user_id match
+    form = RegisterForm(obj=User)
+
+    if form.validate_on_submit():
+        # Update form info for user
+        user = User(name=form.name.data,
+                        username=form.username.data,
+                        email=form.email.data,
+                        role=form.role.data,
+                        dept=form.dept.data)
+        db.session.commit()
+        return redirect(url_for('users'))
+
+    return render_template('modify_user.html', form=form)
+
+
+@app.route('/delete_ticket/<int:ticket_id>', methods=['GET','POST'])
+@login_required
+def delete_ticket(ticket_id):
+    ticket = Ticket.query.get_or_404(ticket_id)  # Grab ticket based on ticket_id match
+
+    db.session.delete(ticket)
+    db.session.commit()  # Save any changes
+
+    return redirect(url_for('tickets'))
+
+@app.route('/delete_user/<int:user_id>', methods=['GET','POST'])
+@login_required
+def delete_user(user_id):
+    user = Ticket.query.get_or_404(user_id)  # Grab user based on user_id match
+
+    db.session.delete(user)
+    db.session.commit()  # Save any changes
+
+    return redirect(url_for('users'))
+
+@app.route('/ticket/<int:ticket_id>')
+def view_ticket(ticket_id):
+    # Retrieve the ticket details from the database based on the ticket_id
+    ticket = Ticket.query.get_or_404(ticket_id)
+    
+    # For simplicity, assuming there's a 'comments' attribute in the Ticket model
+    comments = Comment.query.filter_by(ticket_id=ticket_id).all()
+
+    return render_template('ticket_detail.html', ticket=ticket, comments=comments)
 
 if __name__ == '__main__':
     app.run()
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 class LoginForm(FlaskForm):
@@ -15,15 +16,29 @@ class RegisterForm(FlaskForm):
     email = StringField('Email')
     password = PasswordField('Password', validators=[DataRequired()])
     role = RadioField(choices=[(0, 'Customer'), (1, 'Staff'), (2, 'Admin')], default=0, validators=[DataRequired()])
-    dept = RadioField(choices=[(0, 'Unsure'), (1, 'HR'), (2, 'Marketing'), (3, 'R&D'), (4, 'Development')], default=0, validators=[DataRequired()])
+    dept = RadioField(choices=[(1, 'HR'), (2, 'Marketing'), (3, 'R&D'), (4, 'Development')], default=0,
+                      validators=[DataRequired()])
     submit = SubmitField('Register')
 
 
 class TicketForm(FlaskForm):
+
     created_by = StringField('Created by', validators=[DataRequired()])
-    dept = StringField('Department', validators=[DataRequired()])
+    dept = SelectField('Department', choices=[
+        ('1', 'HR'),
+        ('2', 'Marketing'),
+        ('3', 'R&D'),
+        ('4', 'Development')
+    ], validators=[DataRequired()])
     title = StringField('Title: ', validators=[DataRequired()])
+    assigned_to = StringField('Assigned To', default='admin')   # Default to admin
+    status = SelectField(choices=[('open', 'Open'), ('in-progress', 'In-Progress'), ('closed', 'Closed')])
+    priority = SelectField(choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
     description = StringField('Description')
-    location = StringField('Location')
+    location = SelectField('Location', choices=[
+        ('office1', 'Office 1'),
+        ('office2', 'Office 2'),
+        ('office3', 'Office 3')
+    ])
     attachment = StringField('Attachment(s)')
     submit = SubmitField('Submit Ticket')
